79465c252284f9486894e6bfb5bfa347401b7092
Irena <irena.podgurski@gmail.com>
2020-11-25 20:42:43 +0000

    countOnly function

---
+++ countOnly.js
@@ -0,0 +1,55 @@
+//ASSERTION FUNCTION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💚 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//ACTUAL FUNCTION
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+}
+
+
+//Test Case 1
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
+//Test Cases 2
+const faveFoods = ["pizza", "pasta", "cake", "burger", "fries", "pizza", "pizza"];
+const result2 = countOnly(faveFoods, {"pizza": true, "salad": true, "kiwi": true});
+
+assertEqual(result2["pizza"], 3);
+assertEqual(result2["kiwi"], undefined);
+assertEqual(result2["cake"], undefined);
