dc438b1c3485f2dc2b375a8f17c15cf6a3eb5f33
Irena <irena.podgurski@gmail.com>
2020-11-30 21:11:32 +0000

    refactor with M&C tests

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,31 +1,20 @@
 const tail = require("../tail");
-const assertEqual = require("../assertEqual");
+const { assert } = require("chai");
 
-/*
-Test case: check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-*/
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2 ,3]", () => {
+    assert.deepEqual(tail([1, 2 ,3]), [2, 3])
+  });
 
-//Test case 1: Compare values of the returned array directly
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+  it("returns ['my', 'name', 'is', 'Irena'] for ['hello', 'my', 'name', 'is', 'Irena']", () => {
+    assert.deepEqual(tail(['hello', 'my', 'name', 'is', 'Irena']), ['my', 'name', 'is', 'Irena'])
+  });
 
-//Test case 2: An array with only one element should return an empty array for its tail
-const result1 = tail([1]);
-assertEqual(result1.length, 0);
+  it("returns [] when have an empty array", () => {
+    assert.deepEqual(tail([""]), [])
+  });
 
-//Test case 3: an empty array should return an empty array for its tail
-const result2 = tail([]);
-assertEqual(result2.length, 0);
-
-//Test case 4: Compare values of the returned array directly
-const result3 = tail([1, 2, 3, 4, 5]);
-assertEqual(result3.length, 4); 
-assertEqual(result3[0], 2); 
-assertEqual(result3[1], 3); 
-assertEqual(result3[2], 4); 
-assertEqual(result3[3], 5);
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), [])
+  });
+});
