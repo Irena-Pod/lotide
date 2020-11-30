b9f8775f627d3cad4ca776b0baad868fb3c4d3cb
Irena <irena.podgurski@gmail.com>
2020-11-30 20:39:23 +0000

    function refactor and adding M&C tests

--- test/headTest.js
+++ test/headTest.js
@@ -1,10 +1,25 @@
-
+const { assert } = require("chai");
 const head = require("../head");
 
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-//Test cases
-assertEqual(head(["hi", "bye"]), "hi");
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "");
-assertEqual(head([1]), 1);
+  it("returns 'hi' for ['hi', 'bye']", () => {
+    assert.strictEqual(head(["hi", "bye"]), "hi");
+  });
+
+  it("returns 'hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("returns '' for []", () => {
+    assert.strictEqual(head([""]), "");
+  });
+
+  it("returns 1 for [1]", () => {
+    assert.strictEqual(head([1]), 1);
+  });
+});
+
