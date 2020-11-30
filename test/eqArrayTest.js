8e1806d14d837b48a3c05ea01e1ab1a35eb52303
Irena <irena.podgurski@gmail.com>
2020-11-30 18:03:57 +0000

    eqArrays function refactor and add test file

---
+++ test/eqArrayTest.js
@@ -0,0 +1,8 @@
+const eqArrays = require("../eqArrays");
+const assertEqual = require("../assertEqual")
+
+//assertEqual Test Cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false);// => should FAIL
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);// => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);// => should FAIL
